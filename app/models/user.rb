20e8a72241e7a8d478e2db7d0e5f4ce11d047c36
Mathew Kostiuk <mathewkostiuk@gmail.com>
2017-08-04 02:03:59 +0000

    added validation for register/login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
-
   has_secure_password
 
+  validates :first_name, length: { minimum: 2 }
+  validates :last_name, length: { minimum: 2 }
+  validates :email, uniqueness: true
 end
