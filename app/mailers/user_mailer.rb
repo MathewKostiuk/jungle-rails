46964175bb9327655671d254b5ec902d641d76ee
Mathew Kostiuk <mathewkostiuk@gmail.com>
2017-08-04 01:27:04 +0000

    implemented basic HTTP authentication

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,6 @@ class UserMailer < ApplicationMailer
 
   def order_email(order)
     @order = order
-    mail(to: @order.email, subject: "#{@order.id}")
+    mail(to: @order.email, subject: "Order number: #{@order.id}")
   end
 end
